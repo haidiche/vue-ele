5e95ed292bdb2ebbebfe8c380a4179fbbfa1a275
haidiche <980100177@qq.com>
2021-09-09 15:35:51 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <div >
+        <el-row>
+            <el-col :span="24"><div class="grid-content bg-purple-dark"></div></el-col>
+       </el-row>
+       <el-row>
+            <el-col :span="12"><div class="grid-content bg-purple"></div></el-col>
+            <el-col :span="12"><div class="grid-content bg-purple-light"></div></el-col>
+       </el-row>
+        <el-row :gutter="10">
+            <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+            <el-col :span="8"><div class="grid-content bg-purple-light"></div></el-col>
+            <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+       </el-row>
+        <el-row>
+            <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+            <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+            <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+            <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+       </el-row>
+       <el-row>
+            <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+       </el-row>
+    </div>
+    <div>
+      <router-view></router-view>
+     </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+
+ .el-row {
+    margin-bottom: 30px;
+    &:last-child {
+       margin-left: 0px;
+       margin-top: 40px
+    }
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+</style>
